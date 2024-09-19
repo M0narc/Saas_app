74973c12407a587207e3d941f46eb01ed0aa6d3c
M0narc <francomatiaslujan23@gmail.com>
2024-09-18 22:54:43 -0300

    .env for secrets

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Saas_app
 Building fundations for Software as a Service business using Django, Stripe, Neon PostgreSQL, TailwindCSS, GitHub Actions.
+Railway: https://railway.app/dashboard
+
+go to `https://saasapp-production-6fcd.up.railway.app/`
 
 # goal
 to learn how to create a reusable SaaS fundation
--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u40qsd%=fo6p1zs=4^%=0oi4qzhi0punx2k*y*w9io%yw$o2xw'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
