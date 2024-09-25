84c93ff9535cf31750573a7eb0b2bc3eb5332390
M0narc <francomatiaslujan23@gmail.com>
2024-09-25 13:23:24 -0300

    loggers and pep8 fix

--- src/management/management/commands/vendor_static_pull.py
+++ src/management/management/commands/vendor_static_pull.py
@@ -1,8 +1,11 @@
 import helpers
 
+import logging
 from typing import Any
 from django.conf import settings
 from django.core.management.base import BaseCommand
+
+logging.basicConfig(filename="staticfile_downloads.log", level=logging.ERROR)
 
 STATICFILES_VENDOR_DIR =  getattr(settings, 'STATICFILES_VENDOR_DIR')
 
@@ -17,13 +20,17 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str | None:
         self.stdout.write("Downloading vendor static files")
         completed_url = []
+
         for name, url in VENDOR_STATICFILES.items():
             out_path = STATICFILES_VENDOR_DIR / name
-            dl_success = helpers.download_to_local(url, out_path)
-            if dl_success:
-                completed_url.append(url)
-            else:
-                self.style.ERROR(f'Failed to download {url}')
+            try:
+                dl_success = helpers.download_to_local(url, out_path)
+                if dl_success:
+                    completed_url.append(url)
+            except Exception as e:
+                logging.error(f"Error downloading {url}: {e}")
+                self.stdout.write(self.style.ERROR(f'Error downloading {url}'))
+                
         if set(completed_url) == set(VENDOR_STATICFILES.values()):
             self.stdout.write(
                 self.style.SUCCESS('Successfully updated all vendor static files.')
